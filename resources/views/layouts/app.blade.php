b8038ae7dafeea7ab48afce82c63f0ebc228dc70
wengjoon <107576845+wengjoon@users.noreply.github.com>
2025-04-20 13:02:05 +0700

    new2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('images/favicon/safari-pinned-tab.svg') }}" color="#fe2c55">
+    <meta name="msapplication-TileColor" content="#fe2c55">
+    <meta name="theme-color" content="#fe2c55">
+    
     <!-- Primary Meta Tags -->
     @yield('meta-tags')
     
