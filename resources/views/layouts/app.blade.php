3a6579b514425e34a3d329c6f0f2c802e8e249cb
wengjoon <107576845+wengjoon@users.noreply.github.com>
2025-04-21 17:18:10 +0700

    new8

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,11 @@
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
+    <link rel="shortcut icon" href="{{ asset('images/favicon/favicon.ico') }}">
     <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}">
     <link rel="mask-icon" href="{{ asset('images/favicon/safari-pinned-tab.svg') }}" color="#fe2c55">
     <meta name="msapplication-TileColor" content="#fe2c55">
+    <meta name="msapplication-config" content="{{ asset('images/favicon/browserconfig.xml') }}">
     <meta name="theme-color" content="#fe2c55">
     
     <!-- Primary Meta Tags -->
